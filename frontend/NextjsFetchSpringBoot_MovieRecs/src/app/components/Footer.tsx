5343e60e37585fe961eb5a505eed935b6de4a612
Nickolas Piraino <kwnp17c@gmail.com>
2025-01-20 15:21:38 -0500

    footer is more responsive;

--- frontend/NextjsFetchSpringBoot_MovieRecs/src/app/components/Footer.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/app/components/Footer.tsx
@@ -1,14 +1,13 @@
-// import Link from "next/link"
 import { navItems } from "../resources/navItems"
 import FooterNavItem from "./FooterNavItem"
 import Link from "next/link"
 
 const Footer = () => {
     return (
-        <div className="footBlur relative w-full h-[10vh] border-t-2 border-accent flex items-center text-sm justify-around bg-colour text-contrast">
+        <div className="footBlur relative hidden w-full h-[10vh] border-t-2 border-accent md:flex items-center text-sm lg:text-base xl:text-lg justify-around bg-colour text-contrast">
             <div className="flex flex-col">
                 <p>More than a Movie!</p>
-                <p className="text-xs">© 2024</p>
+                <p className="text-xs xg:text-sm ">© 2024</p>
             </div>
             <div className="">
                 <ul className="underline flex gap-x-2">
