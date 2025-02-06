0f094de4c346398e4ac3f4eac8f8cbd2f1578a3e
Nickolas Piraino <kwnp17c@gmail.com>
2025-02-06 01:52:46 -0500

    put environment variables in a separate local variable and then fetched; for vercel deployment error;

--- frontend/NextjsFetchSpringBoot_MovieRecs/src/app/resources/data.ts
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/app/resources/data.ts
@@ -50,7 +50,9 @@ export async function FindByTitleLike(query?: string, page?: number) {
         query = query ?? "";
         page = page ?? 0;
         const size = query == "" ? 10 : 5;
-        const data = await fetch(`http://${process.env.OTHER_REST_API_IP}:${process.env.REST_API_PORT}/movie/search/findByTitleIgnoreCaseLike?title=${query}&page=${page - 1}&size=${size}`);
+        const ip = process.env.OTHER_REST_API_IP;
+        const port = process.env.REST_API_PORT;
+        const data = await fetch(`http://${ip}:${port}/movie/search/findByTitleIgnoreCaseLike?title=${query}&page=${page - 1}&size=${size}`);
         return data.json();
     } catch (error) {
         throw new Error("Failed to fetch movies like title from Database: " + error);
