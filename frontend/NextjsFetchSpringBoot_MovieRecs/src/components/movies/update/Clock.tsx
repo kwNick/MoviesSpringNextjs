b702c785766cc4cc880814eaf17a88aee7174422
Nickolas Piraino <kwnp17c@gmail.com>
2025-03-14 14:23:02 -0400

    added numbers to the clock;

--- frontend/NextjsFetchSpringBoot_MovieRecs/src/components/movies/update/Clock.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/components/movies/update/Clock.tsx
@@ -2,9 +2,11 @@
 import { useEffect, useRef } from "react";
 
 const Clock = () => {
-    const hour = useRef<HTMLDivElement>(null)
-    const minute = useRef<HTMLDivElement>(null)
-    const second = useRef<HTMLDivElement>(null)
+    const hour = useRef<HTMLDivElement>(null);
+    const minute = useRef<HTMLDivElement>(null);
+    const second = useRef<HTMLDivElement>(null);
+
+    const numbers = useRef<(HTMLDivElement | null)[]>([]);
     const updateClock = () => {
         const now = new Date();
         const h = now.getHours() % 12;
@@ -27,22 +29,40 @@ const Clock = () => {
     };
 
     useEffect(() => {
+        // console.log(numbers.current);
+        // numbers.current.forEach((el, i) => {
+        //     if (el) {
+        //         const deg = (i + 1) * 30;
+        //         el.style.transform = `rotate(${deg}deg)`;
+        //     }
+        // });
+
         const interval = setInterval(updateClock, 1000);
         return () => clearInterval(interval);
 
     }, []);
     return (
-        <div className="clock w-[20vh] h-[20vh] rounded-full border-colour shadow-lg shadow-colour fixed left-[5%] top-[25%]">
+        <div className=" w-[20vh] h-[20vh] rounded-full border-colour shadow-inner shadow-colour fixed left-[5%] top-[25%]">
             <div className="relative w-full h-full ">
 
-                <div className=" absolute rounded-full  bg-accent border border-accent w-[8%] h-[8%] top-[50%] left-[50%] shadow-xl shadow-accent translate-x-[-50%] translate-y-[-50%] " />
+                <div className=" absolute rounded-full bg-[#ef4444]/70 transparent w-[5%] h-[5%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] -z-10" />
 
-                <div ref={hour} className=" absolute w-[3%] h-[20%] bottom-[50%] left-[calc(50%-1.5%)] bg-accent shadow-accent shadow-xl rounded-lg origin-bottom duration-100" />
+                <div ref={hour} className=" absolute w-[2%] h-[20%] bottom-[50%] left-[50%] bg-accent shadow-accent shadow-xl rounded-lg origin-bottom translate-x-[-50%] translate-y-[-50%]" />
 
-                <div ref={minute} className=" absolute w-[2.5%] h-[30%] bottom-[50%] left-[calc(50%-1.25%)] bg-snow shadow-accent shadow-xl rounded-lg origin-bottom duration-100" />
+                <div ref={minute} className=" absolute w-[1.5%] h-[30%] bottom-[50%] left-[50%] bg-snow shadow-snow shadow-xl rounded-lg origin-bottom translate-x-[-50%] translate-y-[-50%]" />
 
-                <div ref={second} className=" absolute w-[2%] h-[35%] bottom-[50%] left-[calc(50%-1%)] bg-colour shadow-accent shadow-xl rounded-lg origin-bottom duration-100" />
+                <div ref={second} className=" absolute w-[1.2%] h-[35%] bottom-[50%] left-[50%] bg-colour shadow-2xl shadow-colour rounded-lg origin-bottom translate-x-[-50%] translate-y-[-50%]" />
 
+                {
+                    Array.from({ length: 12 }).map((_, i) => {
+                        return (
+
+                            <div key={i} ref={(el) => { if (el) numbers.current[i] = el; }} style={{ transform: `rotate(${(i + 1) * 30}deg)` }}
+                                className="absolute w-full h-full text-center text-accent [text-shadow:0_0_3px_var(--accent)]">
+                                <span className="inline-block" style={{ transform: `rotate(${-1 * (i + 1) * 30}deg)` }} >{i + 1}</span>
+                            </div>
+                        )
+                    })}
             </div>
         </div>
     )
