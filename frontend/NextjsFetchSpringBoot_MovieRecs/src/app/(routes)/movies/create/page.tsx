e086ffd28cf743414938232242772ed815d2b88e
Nickolas Piraino <kwnp17c@gmail.com>
2025-03-18 15:53:43 -0400

    redefining the createForm component reference;

--- frontend/NextjsFetchSpringBoot_MovieRecs/src/app/(routes)/movies/create/page.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/app/(routes)/movies/create/page.tsx
@@ -1,4 +1,4 @@
-import CreateForm from "@/components/movies/CreateForm"
+import CreateForm from "@/components/movies/create/CreateForm"
 const page = () => {
     return (
         <div className="w-full min-h-[78vh] flex items-center justify-center">
