e227953ee694a49031905fcb38255340c765e451
Nickolas Piraino <kwnp17c@gmail.com>
2025-03-24 16:45:17 -0400

    checking preffered color scheme and fixing css variables for dark mode

--- frontend/NextjsFetchSpringBoot_MovieRecs/src/components/navbar/LoginSignup.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/components/navbar/LoginSignup.tsx
@@ -6,10 +6,10 @@ const LoginSignup = () => {
     const pathname = usePathname();
     return (
         <>
-            <Link href={"/login"} data-content="LogIn" className={`relative before:absolute before:top-0 before:left-0 before:content-[attr(data-content)] before:text-accent before:w-0 before:border-r-2 before:duration-500 hover:before:w-full hover:before:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] before:overflow-hidden ${pathname == '/login' ? "before:w-full" : ""}`}>
+            <Link href={"/login"} data-content="LogIn" className={`relative hover:text-accent duration-500 before:absolute before:top-0 before:left-0 before:content-[attr(data-content)] before:text-accent before:w-0 before:border-r-2 before:border-r-colour before:duration-500 hover:before:w-[105%] hover:before:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] before:overflow-hidden ${pathname == '/login' ? "before:w-[105%]" : ""}`}>
                 LogIn
             </Link>
-            <Link href={"/signup"} data-content="SignUp" className={`relative before:absolute before:top-0 before:left-0 before:content-[attr(data-content)] before:text-accent before:w-0 before:border-r-2 before:duration-500 hover:before:w-full hover:before:drop-shadow-md before:overflow-hidden ${pathname == '/signup' ? "before:w-full" : ""}`}>
+            <Link href={"/signup"} data-content="SignUp" className={`relative hover:text-accent duration-500 before:absolute before:top-0 before:left-0 before:content-[attr(data-content)] before:text-accent before:w-0 before:border-r-2 before:border-r-colour before:duration-500 hover:before:w-[105%] hover:before:drop-shadow-md before:overflow-hidden ${pathname == '/signup' ? "before:w-[105%]" : ""}`}>
                 SignUp
             </Link>
         </>
--- frontend/NextjsFetchSpringBoot_MovieRecs/src/components/navbar/ThemeToggle.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/components/navbar/ThemeToggle.tsx
@@ -28,7 +28,7 @@ const ThemeToggle = () => {
     };
 
     return (
-        <div className="absolute top-[1%] left-[18%] text-sm" onClick={toggleTheme}>
+        <div className="absolute top-[2%] left-[18%] text-sm border-[1px] border-colour p-1 rounded-xl" onClick={toggleTheme}>
             {darkMode ? "Light" : "Dark"}
         </div>
     )
