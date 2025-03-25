ae969625d7b2489c636c218d173b4a1913412ba9
Nickolas Piraino <kwnp17c@gmail.com>
2025-03-25 02:12:16 -0400

    adding local storage to FavoritesProvider so the favorites data persists across state

--- frontend/NextjsFetchSpringBoot_MovieRecs/src/context/FavoritesContext.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/context/FavoritesContext.tsx
@@ -1,30 +1,45 @@
 'use client';
 import { NewMovie } from "@/resources/definitions";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 // Define the shape of the context
 interface FavoritesContextType {
     favorites: NewMovie[];
-    addFavorite: (item: NewMovie) => void;
-    removeFavorite: (item: NewMovie) => void;
+    addFavorite: (movie: NewMovie) => void;
+    removeFavorite: (movie: NewMovie) => void;
 }
 
+//Create the context
 const FavoritesContext = createContext<FavoritesContextType>({
     favorites: [],
     addFavorite: () => { },
     removeFavorite: () => { },
 });
 
+//Provider component
 export const FavoritesProvider = ({ children }: Readonly<{
     children: React.ReactNode;
 }>) => {
     const [favorites, setFavorites] = useState<NewMovie[]>([]);
 
+    //Load from LocalStorage on initial render
+    useEffect(() => {
+        const storedFavorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+        setFavorites(storedFavorites);
+    }, []);
+
+    //Save to LocalStorage every time favorites change
+    useEffect(() => {
+        localStorage.setItem("favorites", JSON.stringify(favorites));
+    }, [favorites]);
+
+    // Add to favorites
     const addFavorite = (movie: NewMovie) => {
         if (!favorites?.find((fav) => fav.title === movie.title)) {
             setFavorites((prev) => [...prev, movie]);
         }
     };
 
+    // Remove from favorites
     const removeFavorite = (movie: NewMovie) => {
         setFavorites((prev) => prev.filter((fav) => fav.title !== movie.title));
     };
