71fcda90afafecbf5489f2ddc876cf64765ee316
Nickolas Piraino <kwnp17c@gmail.com>
2025-08-04 19:23:52 -0400

    modularizing bubble component for custom top, left, height, width; random movement within bounds of screen on interval;

--- frontend/NextjsFetchSpringBoot_MovieRecs/src/app/(routes)/movies/create/template.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/app/(routes)/movies/create/template.tsx
@@ -4,7 +4,11 @@ export default function Template({ children }: { children: React.ReactNode }) {
     return (
         <>
 
-            <Bubble />
+            <Bubble t={50} l={10} w={20} h={20} />
+            <Bubble t={20} l={70} w={15} h={15} />
+            <Bubble t={80} l={40} w={10} h={10} />
+            <Bubble t={30} l={30} w={25} h={25} />
+            <Bubble t={60} l={80} w={18} h={18} />
             {children}
         </>
     );
--- frontend/NextjsFetchSpringBoot_MovieRecs/src/components/movies/create/Bubble.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/components/movies/create/Bubble.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useRef } from "react";
 
-const Bubble = () => {
+const Bubble = ({ t, l, w, h }: { t: number, l: number, w: number, h: number }) => {
     const bub = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -12,23 +12,36 @@ const Bubble = () => {
                 const viewportHeight = window.innerHeight;
                 // console.log(viewportHeight, viewportWidth);
 
-                const maxX = viewportWidth - bub.current.clientWidth;
-                const maxY = viewportHeight - bub.current.clientHeight;
+                const maxX = (viewportWidth - bub.current.clientWidth) * .75;
+                const maxY = (viewportHeight - bub.current.clientHeight) * .75;
                 // console.log(maxX, maxY);
 
-                const randomX = Math.random() * maxX;
-                const randomY = Math.random() * maxY;
+                //Math.abs(Math.floor(Math.random() * 65));
+                let randomX = Math.abs(Math.floor(Math.random() * maxX / 2));
+                if (randomX + ((l / 100) * viewportWidth) + ((w / 100) * viewportWidth) > maxX) {
+                    while (randomX > maxX) {
+                        randomX = Math.abs(Math.floor(Math.random() * maxX / 2));
+                    }
+                }
+
+                let randomY = Math.abs(Math.floor(Math.random() * maxY / 2));
+                if (randomY + ((t / 100) * viewportHeight) + ((h / 100) * viewportHeight) > maxY) {
+                    while (randomY > maxY) {
+                        randomY = Math.abs(Math.floor(Math.random() * maxY / 2));
+                    }
+                }
                 // console.log(randomX, randomY);
 
-                bub.current.style.transform = `translate: ${randomX}px, ${randomY}px`;
+                bub.current.style.transform = `translate( ${randomX}px, ${randomY}px)`;
             }
         };
-        const interval = setInterval(moveBubble, 2000);
+        const timeDelay = Math.abs(Math.floor(Math.random() * 2001) + 1000);
+        const interval = setInterval(moveBubble, timeDelay);
 
         return () => clearInterval(interval); // Cleanup on unmount
-    }, []);
+    },);
     return (
-        <div ref={bub} className="absolute top-[50%] left-[10%] w-[20vw] h-[20vw] rounded-full [zoom:_0.35] [box-shadow:_inset_0_0_25px_rgba(255,255,255,0.25),_0_0_25px_rgba(255,255,255,0.25)] before:absolute before:top-[12%] before:left-[26%] before:w-[17%] before:h-[17%] before:rounded-full before:bg-white before:[filter:_blur(2px)] before:z-10 after:absolute after:top-[30.5%] after:left-[41.5%] after:w-[10%] after:h-[10%] after:rounded-full after:bg-white after:[filter:_blur(2px)] after:z-10 transition-transform duration-[2s]">
+        <div ref={bub} className={`absolute rounded-full pointer-events-none -z-10 [zoom:_0.35] [box-shadow:_inset_0_0_25px_rgba(255,255,255,0.25),_0_0_25px_rgba(255,255,255,0.25)] before:absolute before:top-[12%] before:left-[26%] before:w-[17%] before:h-[17%] before:rounded-full before:bg-white before:[filter:_blur(2px)] before:z-10 after:absolute after:top-[30.5%] after:left-[41.5%] after:w-[10%] after:h-[10%] after:rounded-full after:bg-white after:[filter:_blur(2px)] after:z-10 transition-transform ease-in-out duration-[2.5s]`} style={{ top: `${t}%`, left: `${l}%`, width: `${w}vw`, height: `${h}vw` }}>
 
             <span className="absolute inset-0 rounded-full border-l-2 border-l-snow [filter:_blur(3px)]" />
             <span className="absolute inset-0 rounded-full border-r-4 border-r-accent [filter:_blur(3px)]" />
