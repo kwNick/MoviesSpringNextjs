0329b03f4a218f78b5e1577824a923cd7ec56619
Nickolas Piraino <kwnp17c@gmail.com>
2025-08-14 19:41:34 -0400

    fixing theme toggle;

--- frontend/NextjsFetchSpringBoot_MovieRecs/src/components/navbar/ThemeToggle.tsx
+++ frontend/NextjsFetchSpringBoot_MovieRecs/src/components/navbar/ThemeToggle.tsx
@@ -6,29 +6,49 @@ const ThemeToggle = () => {
 
     // Load dark mode state from localStorage on mount
     useEffect(() => {
-        if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            setDarkMode(true);
-            document.body.classList.toggle("dark", true);
-        }
+        const root = document.documentElement;
+        // root.classList.add("transition-colors", "duration-500", "ease-in-out");
 
+        // Check localStorage for dark mode preference on mount
         if (typeof window !== "undefined") {
             const storedTheme = localStorage.getItem("darkMode");
             if (storedTheme) {
-                setDarkMode(JSON.parse(storedTheme));
-                document.body.classList.toggle("dark", JSON.parse(storedTheme));
+                const theme = JSON.parse(storedTheme);
+                setDarkMode(theme);
+                root.classList.toggle("dark", theme);
+                return;
             }
         }
+
+        // Check System preference for dark mode
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            setDarkMode(true);
+            root.classList.toggle("dark", true);
+        }
+
+        // 3. Watch for system preference changes
+        const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+        const handleChange = (e: MediaQueryListEvent) => {
+        if (!localStorage.getItem("darkMode")) { // only auto-change if no saved choice
+            setDarkMode(e.matches);
+            root.classList.toggle("dark", e.matches);
+        }
+        };
+        mediaQuery.addEventListener("change", handleChange);
+
+        return () =>{ mediaQuery.removeEventListener("change", handleChange)};
+
     }, []);
 
     const toggleTheme = () => {
         const theme = !darkMode;
         setDarkMode(theme);
         localStorage.setItem("darkMode", JSON.stringify(theme));
-        document.body.classList.toggle("dark", theme);
+        document.documentElement.classList.toggle("dark", theme);
     };
 
     return (
-        <div className="absolute top-[2%] left-[18%] text-sm border-[1px] border-colour p-1 rounded-xl" onClick={toggleTheme}>
+        <div className="absolute top-[2%] left-[18%] text-sm border-[1px] border-colour p-1 rounded-xl cursor-pointer" onClick={toggleTheme}>
             {darkMode ? "Light" : "Dark"}
         </div>
     )
